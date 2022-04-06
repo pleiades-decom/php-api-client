c98110ffcd9740b99f6e26211016474b173db18b
Dusan <dusan.daniska@wai.sk>
2022-04-06 07:35:42 +0200

    .

--- src/Client.php
+++ src/Client.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * PLEIADES PHP API Client
+ * Library to access the PLEIADES API.
+ *
+ * Author: Dusan Daniska, dusan.daniska@wai.sk
+ *
+ * License: See LICENSE.md file in the root folder of the software package.
+ */
+
 namespace PleiadesDecom\PhpApiClient;
 
 class Client {
